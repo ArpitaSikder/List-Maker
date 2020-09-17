69c7ec6e12fb048b62b9f09391fac7055652d889
Arpita Sikder <34753366+ArpitaSikder@users.noreply.github.com>
2020-09-17 18:26:34 -0400

    Add files via upload

--- ToDoList.js
+++ ToDoList.js
@@ -1,65 +1,114 @@
-var array;
-array = new Array();
-
 function appendText() {
   //adds elements
+  var array = new Array();
+  console.log(array);
   var txt1 = document.getElementById("insert").value;
+  console.log(txt1);
   $(document).ready(function () {
-    var str = "=";
     if (txt1.indexOf("=") != -1) {
       //checks if the format is right [note: didnot use .includes because internet explorer do not support it]
       //txt1 = txt1.replace(" ", ""); //replaces any spaces from userinput
       array.push(txt1); //collects the items in an array for using it in the sortbytask/sortbydate functions
+
       console.log(array); //It was added for debugging
+
       $("#select").append("<li>" + txt1 + "</li>"); //adds the items
     } else {
       alert(
-        "Please enter text in the format 'task=date' eg. gardening=3rd Sept"
+        "Please enter text in the format 'task=date(must be numeric)' eg. gardening=3"
       ); //alerts user about the wrong formatting if any
     }
   });
 }
 
 function sortbytask() {
-  //sorts by task which is the first part of the pair
   var i;
-  var array_sorted = new Array();
-  array_sorted = array.sort(); //sorts the array alphabetically
-  console.log(array_sorted); //for debugging
+  var array = new Array();
+  var array_init = new Array();
+  var hashtable = {};
+  var div = document.getElementById("select");
+  console.log(div.childNodes[0].textContent);
+  for (i = 0; i < div.childNodes.length; i++) {
+    if (div.childNodes[i].value != null) {
+      array.push(div.childNodes[i].textContent);
+    }
+  }
+  console.log("HEEEE");
+  console.log(array);
+  //sorts by task which is the first part of the pair
+
+  for (var s in array) {
+    var key = array[s].substr(0, array[s].indexOf("="));
+    var value = array[s].substr(array[s].indexOf("=") + 1, array[s].length);
+    console.log(key);
+    console.log(value);
+    if (hashtable[key] == null) {
+      console.log("here");
+      var arr = new Array();
+      arr.push(value);
+      hashtable[key] = arr;
+      array_init.push(key);
+    } else {
+      hashtable[key].push(value);
+    }
+  }
+  array_init.sort();
   $("#select").empty(); //clears the content first before showing the newly sorted list
-  console.log(array_sorted);
-  for (i = 0; i < array_sorted.length; i++) {
+  for (i = 0; i < array_init.length; i++) {
     //for loop is used to add all the items to the list
-    $("#select").append("<li>" + array_sorted[i] + "</li>");
+    var v = hashtable[array_init[i]].toString().split(",");
+    for (var k in v) {
+      if (array_init[i] != null && v[k] != null) {
+        var result = array_init[i] + "=" + v[k];
+        $("#select").append("<li>" + result + "</li>");
+      }
+    }
   }
 }
 
 function sortbydate() {
-  //sorts by date which the second part of the pair, here the first part is not important for sorting
-  var i,
-    j = 0;
-  var array_value = new Array();
-  var array_valuesorted = new Array();
-  for (i = 0; i < array.length; i++) {
-    var temp_no = array[i].search("="); //find the index where the ('=')/equals to sign exit
-    var temp =
-      array[i].slice(temp_no + 1, array[i].length) +
-      "=" +
-      array[i].slice(0, temp_no); //changes the string in the format date=task (oppsite) which will help for sorting
-    array_value.push(temp); //a new array is used to added the changed formatted list items
+  var i;
+  var array = new Array();
+  var array_init = new Array();
+  var hashtable = {};
+  var div = document.getElementById("select");
+
+  // console.log(div.childNodes[0].textContent);
+  for (i = 0; i < div.childNodes.length; i++) {
+    if (div.childNodes[i].value != null) {
+      array.push(div.childNodes[i].textContent);
+    }
   }
-  console.log(array_value);
-  array_valuesorted = array_value.sort(); //sorts the items using the date
-  console.log("arrvalue: " + array_valuesorted);
-  $("#select").empty();
-  for (i = 0; i < array_valuesorted.length; i++) {
-    //now that the list is sorted by date, the format is changed back to the desired one
-    var temp1_no = array_valuesorted[i].search("="); //find the index where the ('=')/equals to sign exit
-    var temp1 =
-      array_valuesorted[i].slice(temp1_no + 1, array_valuesorted[i].length) +
-      "=" +
-      array_valuesorted[i].slice(0, temp1_no); //change to the desired format
-    $("#select").append("<li>" + temp1 + "</li>"); //add the items sorted by date
+  console.log("HBBBBB");
+  console.log(array);
+  for (var s in array) {
+    var value = array[s].substr(0, array[s].indexOf("="));
+    var key = array[s].substr(array[s].indexOf("=") + 1, array[s].length);
+    console.log(key);
+    console.log(value);
+    if (hashtable[key] == null) {
+      console.log("here");
+      var arr = new Array();
+      arr.push(value);
+      hashtable[key] = arr;
+      array_init.push(key);
+    } else {
+      hashtable[key].push(value);
+    }
+  }
+  array_init.sort(function (a, b) {
+    return a - b;
+  });
+  $("#select").empty(); //clears the content first before showing the newly sorted list
+  for (i = 0; i < array_init.length; i++) {
+    //for loop is used to add all the items to the list
+    var v = hashtable[array_init[i]].toString().split(",");
+    for (var k in v) {
+      if (array_init[i] != null && v[k] != null) {
+        var result = array_init[i] + "=" + v[k];
+        $("#select").append("<li>" + result + "</li>");
+      }
+    }
   }
 }
 
@@ -70,23 +119,22 @@ function edit() {
     //the for loop iterates through thr childNodes
     var classname = div.childNodes[i].className;
     var values = div.childNodes[i].textContent;
+    // var index = array.indexOf(values);
     if (classname == "ui-selectee ui-selected") {
       var new_value = prompt("Enter the new value");
       if (new_value.indexOf("=") != -1) {
         var new_node = document.createElement("li");
         var new_nodevalue = document.createTextNode(new_value);
         new_node.appendChild(new_nodevalue);
-        array[i - 1] = new_value; //collects the items in an array for using it in the sortbytask/sortbydate functions
         document
           .getElementById("select")
           .replaceChild(new_node, div.childNodes[i]);
       } else {
         alert(
-          "Please enter text in the format 'task=date' eg. gardening=3rd Sept"
+          "Please enter text in the format 'task=date(must be numeric)' eg. gardening=3"
         ); //alerts user about the wrong formatting if any
       }
     }
-    console.log("here");
   }
 }
 
@@ -98,16 +146,12 @@ function deletelist() {
   for (i = 0; i < div.childNodes.length; i++) {
     //the for loop iterates through thr childNodes
     var classname = div.childNodes[i].className;
-    var values = div.childNodes[i].textContent;
-    console.log("cname: " + classname);
-    console.log("values: " + values);
     if (classname == "ui-selectee ui-selected") {
       //check which item is selected by the user
       // note : div.childNodes[i].remove() does not work with internet explorer
       div.removeChild(div.childNodes[i]); //the slected items are removed only
-      i = i - 1; //since the items are removed the list is getting smaller by 1 each time so the index needs to be reduced by 1
-      array.splice(array.indexOf(values), 1); //remove the deleted items from the array, it is necessary because the sortsbytask and date are using this array
-      console.log("farr: " + array);
+      //i = i - 1; //since the items are removed the list is getting smaller by 1 each time so the index needs to be reduced by 1
+      //remove the deleted items from the array, it is necessary because the sortsbytask and date are using this array
     }
   }
 }
